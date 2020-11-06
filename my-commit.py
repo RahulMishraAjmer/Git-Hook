2a5fa7fed374f4be2383ab1d69387a9c491ae358
RahulMishraAjmer <41458266+RahulMishraAjmer@users.noreply.github.com>
2020-11-06 23:47:51 +0530

    Update my-commit.py
    fixes

--- my-commit.py
+++ my-commit.py
@@ -1,17 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 
 def main ():
-    with open(sys.argv[1],"r") as fp:
-	lines = fp.readlines()
-	for line in lines:
-            if line[0]=="keyword"
-		print("Keyword found")
-		sys.exit(0)
-	    else:
-		print("Keyword NOT found")
-		sys.exit(1)
+        with open(sys.argv[1],"r") as fp:
+                lines = fp.readlines()
+                my_str = "bug"
+                if lines[0].lower().find("bug")== 0:
+                        print("Keyword found")
+                else:
+                        print("Keyword NOT found")
+        sys.exit(1)
 
- if __name__=="__main__":
-	main()
+if __name__=="__main__":
+        main()
